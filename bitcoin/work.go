5d1cf1768b81976b46e195e84e7086f3f20b7569
traaitt | TRRXITTE <traaitt@icloud.com>
2025-03-21 05:08:25 +0100

    ~

--- bitcoin/work.go
+++ bitcoin/work.go
@@ -1,38 +1,20 @@
-package pool
+package bitcoin
 
-import (
-    "designs.capital/dogepool/bitcoin"
-)
+// Interface to stratum JSON work packets
+type Work []any
 
-func (pool *PoolServer) generateWorkFromCache(clean bool) (bitcoin.Work, error) {
-    primaryName := pool.config.BlockChainOrder[0]
-    auxNames := pool.config.BlockChainOrder[1:]
+func (b BitcoinBlock) NonceSubmissionSlot() (slotID int) {
+	return 4
+}
 
-    // Fetch template and aux blocks
-    primaryChain := pool.activeNodes[primaryName]
-    template := primaryChain.GetTemplate() // Assume this exists
-    auxBlocks := make(map[string]*bitcoin.AuxBlock)
-    for _, auxName := range auxNames {
-        auxChain := pool.activeNodes[auxName]
-        auxBlock, err := auxChain.GetAuxBlock() // Assume this exists
-        if err != nil {
-            return nil, err
-        }
-        auxBlocks[auxName] = auxBlock
-    }
+func (b BitcoinBlock) NonceTimeSubmissionSlot() (slotID int) {
+	return 3
+}
 
-    // Generate work
-    _, work, err := bitcoin.GenerateWork(
-        template,
-        auxBlocks,
-        primaryName,
-        "", // arbitrary
-        "", // rewardPubScriptKey (adjust if available)
-        0,  // extranonceByteReservationLength
-    )
-    if err != nil {
-        return nil, err
-    }
+func (b BitcoinBlock) Extranonce2SubmissionSlot() (slotID int, exists bool) {
+	return 2, true
+}
 
-    return work, nil
+func (b BitcoinBlock) ShareMultiplier() float64 {
+	return b.chain.ShareMultiplier()
 }
