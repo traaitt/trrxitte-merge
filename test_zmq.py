a8184ab45aed13d26788783e804af6f6f9426503
traaitt | TRRXITTE <traaitt@icloud.com>
2025-03-21 03:50:18 +0100

    add bellscoin port

--- test_zmq.py
+++ test_zmq.py
@@ -1,8 +1,8 @@
 import zmq
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://127.0.0.1:28334")
+socket.connect("tcp://127.0.0.1:1226")
 socket.setsockopt_string(zmq.SUBSCRIBE, "")
-print("Listening on 28334...")
+print("Listening on 1226...")
 while True:
     print(socket.recv())
